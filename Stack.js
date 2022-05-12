e53d0118187d6331381dfc572c40bb27fc25f65a
Alchemy <alchemyzjk@gmail.com>
2022-05-12 10:00:46 +0800

    update stack with Singly LinkedList implementation.

--- Stack.js
+++ Stack.js
@@ -1,4 +1,5 @@
-class Stack {
+// implemented by JS built-in array
+class StackByArray {
   constructor() {
     this.items = [];
   }
@@ -29,5 +30,46 @@ class Stack {
       return null;
     }
     return this.items[this.items.length - 1];
+  }
+}
+
+// implemented by Singly LinkedList
+class ListNode {
+  constructor(val, next=null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class StackByLinkedList {
+  constructor() {
+    this.top = null;
+  }
+
+  isEmpty() {
+    return this.top === null;
+  }
+
+  push(val) {
+    const node = new ListNode(val, this.top);
+    this.top = node;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      console.error('Stack is Empty.');
+      return null;
+    }
+    const node = this.top;
+    this.top = node.next;
+    return node.val;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      console.error('Stack is Empty.');
+      return null;
+    }
+    return this.top.val;
   }
 }
